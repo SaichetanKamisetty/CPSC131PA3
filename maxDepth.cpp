03c09946f53fe64bf2d0c25b9473e4d03946a1d4
SaichetanKamisetty <90051818+SaichetanKamisetty@users.noreply.github.com>
2022-11-13 22:13:20 -0800

    Update maxDepth.cpp

--- maxDepth.cpp
+++ maxDepth.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        // Base case, returns 0 after reaching end of the BST.
         if (root == NULL) {
             return 0;
         } else {
-            return 1 + max(maxDepth(root->right), maxDepth(root->left));
+         // Recursive case, uses recursive DST to find the maxDepth of each subtree and then compares them and returns it.
+         // the + 1 is added for the root, since it isn't included when originally doing the recursion.
+            return max(maxDepth(root->right) + 1, maxDepth(root->left) + 1);
         }
     }
 };

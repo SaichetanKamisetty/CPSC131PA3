a210e78c7bfc768d29fa5216e15a15a62bc1e2ea
SaichetanKamisetty <90051818+SaichetanKamisetty@users.noreply.github.com>
2022-11-13 21:59:42 -0800

    Update searchBST.cpp

--- searchBST.cpp
+++ searchBST.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
+        // Base case: return NULL if val is not found.
         if (root == NULL) {
             return NULL;
+        // Recursive Cases:
+        // If val of current node is greater than val, traverse left in the tree.
         } else if (root->val > val) {
             return searchBST(root->left, val);
+        // If val of current node is less than val, traverse right in the tree.
         } else if (root->val < val) {
             return searchBST(root->right, val);
+        // Otherwise, value is found and return the current node it is on.
         } else {
             return root;
         }
